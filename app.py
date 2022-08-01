0e6666cfba8b56325d6fd42a4ce9949d79153772
ErickDasaev <31356879+ErickDasaev@users.noreply.github.com>
2022-08-01 15:59:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from calcs import *
 
 st.title('Medidas de Riesgo Fiscal')
 
-invoices = pd.read_csv('data/invoices.csv')
-direct_debit = pd.read_csv('data/ddebit.csv')
-contracts_data = pd.read_csv('data/contracts.csv')
-companies_all = pd.read_csv('data/companies.csv')
+invoices = pd.read_csv('data/datainvoices.csv')
+direct_debit = pd.read_csv('data/dataddebit.csv')
+contracts_data = pd.read_csv('data/datacontracts.csv')
+companies_all = pd.read_csv('data/datacompanies.csv')
 
 st.write('Empresas:')
 
