c2d32aac3d8b2b238dbd2e70c51d03b944a75d8e
ErickDasaev <31356879+ErickDasaev@users.noreply.github.com>
2022-08-01 15:57:26 -0500

    Filtered

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ direct_debit = pd.read_csv('data/ddebit.csv')
 contracts_data = pd.read_csv('data/contracts.csv')
 companies_all = pd.read_csv('data/companies.csv')
 
-option = st.sidebar.selectbox('Companies', tuple(set(companies_all['registered_name'].unique())))
+st.write('Empresas:')
 
+option = st.sidebar.selectbox('Companies', tuple(set(companies_all['business_name'].unique())))
+optionid = companies_all[companies_all['business_name']==option].iloc[0]['id']
 st.write('Contratos:')
 st.dataframe(data=contracts_data , width=None, height=None)
