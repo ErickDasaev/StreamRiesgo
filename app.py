caa5089d4bd795a501228dbf4eaa5a9b6f5e0550
ErickDasaev <31356879+ErickDasaev@users.noreply.github.com>
2022-08-01 16:19:48 -0500

    Ana

--- app.py
+++ app.py
@@ -9,10 +9,16 @@ invoices = pd.read_csv('data/datainvoices.csv')
 direct_debit = pd.read_csv('data/dataddebit.csv')
 contracts_data = pd.read_csv('data/datacontracts.csv')
 companies_all = pd.read_csv('data/datacompanies.csv')
+outData = pd.read_csv('data/contractsSummary.csv')
 
 st.write('Empresas:')
 
 option = st.sidebar.selectbox('Companies', tuple(set(companies_all['business_name'].unique())))
-optionid = companies_all[companies_all['business_name']==option].iloc[0]['id']
+optionid = companies_all[companies_all['business_name'] == option].iloc[0]['id']
 st.write('Contratos:')
-st.dataframe(data=contracts_data[contracts_data['company_id']==optionid], width=None, height=None)
+st.dataframe(data=contracts_data[contracts_data['company_id'] == optionid], width=None, height=None)
+
+st.write('Facturas:')
+st.dataframe(data=invoices[invoices['company_id'] == optionid], width=None, height=None)
+
+conInfo = outData[outData['company_id'] == optionid].iloc[0]
