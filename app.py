dc6c9f21203e30dda2cf9acc8490e479a0ea1fd3
ErickDasaev <31356879+ErickDasaev@users.noreply.github.com>
2022-08-01 16:40:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,37 +21,43 @@ st.dataframe(data=contracts_data[contracts_data['company_id'] == optionid], widt
 st.write('Facturas:')
 st.dataframe(data=invoices[invoices['company_id'] == optionid], width=None, height=None)
 
-conInfo = outData[outData['company_id'] == optionid].iloc[0]
 
-for index, value in conInfo.loc[['sat_declaration_rate',
-           'total_sat_rate',
-            'avr_cf_l3m',
-            'avr_cf_l6m',
-            'avr_cf_l12m',
-            'avr_gross_m_l3m',
-            'avr_gross_m_l6m',
-            'avr_gross_m_l12m',
-            'avr_net_m_l3m',
-            'avr_net_m_l6m',
-            'avr_net_m_l12m',
-            'av_itmes_l3m',
-            'av_itmes_l6m',
-            'av_itmes_l12m',
-            'av_amount_l3m',
-            'av_amount_l6m',
-            'av_amount_l12m',
-            'moratory_av_itmes_l3m',
-           'moratory_av_itmes_l6m',
-           'moratory_av_itmes_l12m',
-           'moratory_av_amount_l3m',
-           'moratory_av_amount_l6m',
-           'moratory_av_amount_l12m',
-            'meli_fin_var',
-            'peg_invoicing',
-            'concentration_clients',
-            'data_sat_days',
-            'l3m_total_employees',
-        'l6m_total_employees',
-        'l12m_total_employees']].items():
-    st.markdown(index)
-    st.subheader(value)
+if len(outData[outData['company_id'] == optionid]) > 0:
+
+    conInfo = outData[outData['company_id'] == optionid].iloc[0]
+
+    for index, value in conInfo.loc[['sat_declaration_rate',
+               'total_sat_rate',
+                'avr_cf_l3m',
+                'avr_cf_l6m',
+                'avr_cf_l12m',
+                'avr_gross_m_l3m',
+                'avr_gross_m_l6m',
+                'avr_gross_m_l12m',
+                'avr_net_m_l3m',
+                'avr_net_m_l6m',
+                'avr_net_m_l12m',
+                'av_itmes_l3m',
+                'av_itmes_l6m',
+                'av_itmes_l12m',
+                'av_amount_l3m',
+                'av_amount_l6m',
+                'av_amount_l12m',
+                'moratory_av_itmes_l3m',
+               'moratory_av_itmes_l6m',
+               'moratory_av_itmes_l12m',
+               'moratory_av_amount_l3m',
+               'moratory_av_amount_l6m',
+               'moratory_av_amount_l12m',
+                'meli_fin_var',
+                'peg_invoicing',
+                'concentration_clients',
+                'data_sat_days',
+                'l3m_total_employees',
+            'l6m_total_employees',
+            'l12m_total_employees']].items():
+        st.markdown(index)
+        st.subheader(value)
+
+    else:
+        st.header('No contract was found for this customer')
